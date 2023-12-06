239ef2006df37df2b54d09b3b103f62758771790
Anna Golo <a.frontdevg@gmail.com>
2023-12-06 15:08:50 -0500

    Added featur for opening, editing and closing popup window in profile summury section

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+//Выбор элементов
 const likeButtons = document.querySelectorAll('.cards__button-like');
+const deleteButtons = document.querySelectorAll('.cards__button-delete');
+const editButton = document.querySelector('.profile__button-edit');
+const closeButton = document.querySelector('.popup__button-close');
+const popupElement = document.querySelector('.popup');
+
 
 //Функция активации кнопки cards__button-like.
   likeButtons.forEach(function(likeButton) {
@@ -7,8 +13,6 @@ const likeButtons = document.querySelectorAll('.cards__button-like');
   });
 
 });
-
-const deleteButtons =document.querySelectorAll('.cards__button-delete');
 
 //функция для удаления карточек при нажатии на кнпку
 
@@ -19,3 +23,49 @@ deleteButtons.forEach(function(deleteButton) {
   });
 
 });
+
+//Функция для открытия модального окна редактирования информации о пользователе
+
+editButton.addEventListener('click', function() {
+  popupElement.classList.add('popup_opened');
+
+});
+
+//Функция для закртия модального окна при нажатии на крестик
+
+closeButton.addEventListener('click', function() {
+  popupElement.classList.remove('popup_opened')
+
+});
+
+//Сохранение данных
+
+// Находим форму в DOM
+const formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__form-item_name');
+let jobInput = document.querySelector('.popup__form-item_description');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    let nameValue = nameInput.value;
+    let jobValue = jobInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+   let profileName = document.querySelector('.profile__name');
+   let profileDescription = document.querySelector('.profile__description');
+
+    // Вставьте новые значения с помощью textContent
+    profileName.textContent = nameValue;
+    profileDescription.textContent = jobValue;
+
+    popupElement.classList.remove('popup_opened')
+
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
